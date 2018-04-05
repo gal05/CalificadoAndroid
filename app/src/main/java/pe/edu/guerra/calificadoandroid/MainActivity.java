2e1f53804e7aad5fd6c71066dabbc28c842a803a
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-04-05 15:36:15 -0500

    splash done!

--- app/src/main/java/pe/edu/guerra/calificadoandroid/MainActivity.java
+++ app/src/main/java/pe/edu/guerra/calificadoandroid/MainActivity.java
@@ -1,5 +1,7 @@
 package pe.edu.guerra.calificadoandroid;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        }, 3000);
+
+
+
     }
 }
